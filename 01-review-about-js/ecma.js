52b621e2d13b8760e8cdef81c0eaf50541506af3
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-04 10:48:43 -0400

    feat: Add optional chaining examples for accessing nested properties

--- 01-review-about-js/ecma.js
+++ 01-review-about-js/ecma.js
@@ -3,3 +3,21 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Map, filter, etc.
 const even = arr.filter((number) => number % 2 === 0);
 console.log(even);
+
+// Optional chaining
+const firstStudent = {
+    name: 'John',
+    age: 29,
+    passport: {
+        number: 123456,
+        country: 'USA',
+    },
+};
+
+const secondStudent = {
+    name: 'John',
+    age: 29,
+};
+
+console.log(firstStudent.passport.country);
+console.log(secondStudent.passport.country);
