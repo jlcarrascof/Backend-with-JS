ff21506ecd9e4138a5553f52e0c675718c63901b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-05 09:25:50 -0400

    chore: Add examples of primitive data types, arrays, objects, undefined, and null in primitive.js file

--- 01-review-about-js/primitive.js
+++ 01-review-about-js/primitive.js
@@ -6,3 +6,19 @@ const thisIsAString = 'hello';
 
 // Boolean
 const thisIsABoolean = true;
+
+// Array
+const firstArray = [1, 2, 3];
+const secondArray = [1, 'a', false];
+
+// Object
+const thisIsAnObject = {
+    name: 'John Doe',
+    age: 30,
+};
+
+// Undefined
+const thisIsUndefined = undefined;
+
+// Null
+const thisIsNull = null;
