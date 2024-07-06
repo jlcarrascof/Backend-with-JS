3d664d98b601a868d6b1b363ddaaa26021ba17e2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-06 19:54:35 -0400

    Refactor Person prototype to include multiple instances

--- 01-review-about-js/prototypes.js
+++ 01-review-about-js/prototypes.js
@@ -7,9 +7,11 @@ Person.prototype.sayHello = function() {
 };
 
 const person = new Person('John Doe');
+const person2 = new Person('Jane Doe');
 
 console.log(person); // Person { name: 'John Doe' }
 console.log(person.name); // John Doe
 console.log(person.sayHello()); // Hello, my name is John Doe
 console.log(person.prototype); // undefined
 console.log(person.__proto__); // { sayHello: [Function] }
+console.log(person2.sayHello()); // Hello, my name is Jane Doe
