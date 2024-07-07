2cb47b38d61176b1b701bbfb85ce63ed3b0abca8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-06 21:54:55 -0400

    feat: Add new method to Employee prototype

--- 01-review-about-js/prototypeInheritance.js
+++ 01-review-about-js/prototypeInheritance.js
@@ -14,3 +14,9 @@ function Employee (name, position) {
 // Employee inherits from Person
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
+
+// Add a new method, specific from Employee
+
+Employee.prototype.sayPosition = function() {
+    console.log(`Hello, my name is ${this.name} and I am a ${this.position}`);
+};
