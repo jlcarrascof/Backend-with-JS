05fd2b6faba61aafcb25d307e6268ed3787108f6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-06 21:52:13 -0400

    Refactor Employee constructor to properly inherit from Person prototype

--- 01-review-about-js/prototypeInheritance.js
+++ 01-review-about-js/prototypeInheritance.js
@@ -10,3 +10,7 @@ function Employee (name, position) {
     Person.call(this, name);
     this.position = position;
 }
+
+// Employee inherits from Person
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
