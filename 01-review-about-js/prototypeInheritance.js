9169b9c30a6211a6063f14023b262da4c7fd3ed2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-06 21:49:51 -0400

    Refactor Employee constructor to inherit from Person prototype

--- 01-review-about-js/prototypeInheritance.js
+++ 01-review-about-js/prototypeInheritance.js
@@ -1,4 +1,12 @@
-function Employee (name, position) {
+function Person(name) {
     this.name = name;
+}
+
+Person.prototype.sayHello = function() {
+    return `Hello, my name is ${this.name}`;
+};
+
+function Employee (name, position) {
+    Person.call(this, name);
     this.position = position;
 }
