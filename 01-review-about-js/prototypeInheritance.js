1149b8876e2986c04512e7b7acba32e4c0dd250b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-08 13:02:35 -0400

    Refactor Employee prototype inheritance

--- 01-review-about-js/prototypeInheritance.js
+++ 01-review-about-js/prototypeInheritance.js
@@ -3,6 +3,7 @@ function Person(name) {
 }
 
 Person.prototype.sayHello = function() {
+    console.log(`Hello, my name is ${this.name}`);
     return `Hello, my name is ${this.name}`;
 };
 
@@ -16,7 +17,14 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 // Add a new method, specific from Employee
-
 Employee.prototype.sayPosition = function() {
     console.log(`Hello, my name is ${this.name} and I am a ${this.position}`);
 };
+
+const ana = new Employee('Ana', 'Developer');
+
+// Call the method from Person
+ana.sayHello(); // Hello, my name is Ana
+
+// Call the method from Employee
+ana.sayPosition(); // Hello, my name is Ana and I am a Developer
