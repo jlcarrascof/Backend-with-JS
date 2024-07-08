476be1fc30291f789b48f1da471c28ed9ccb9d56
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-08 13:45:58 -0400

    feat: Add like and dislike methods to LikesService prototype

--- 01-review-about-js/prototypesLikes.js
+++ 01-review-about-js/prototypesLikes.js
@@ -5,3 +5,11 @@ function LikesService() {
   this.likes = 0;
   this.dislikes = 0;
 }
+
+LikesService.prototype.like = function() {
+  this.likes++;
+};
+
+LikesService.prototype.dislike = function() {
+    this.dislikes++;
+};
